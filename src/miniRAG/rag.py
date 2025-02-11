7cfea306581d705a133a3784404fce6e0b915690
SimonCao1207 <hainamkaist@gmail.com>
2025-02-11 16:19:25 +0000

    add logger

--- src/miniRAG/rag.py
+++ src/miniRAG/rag.py
@@ -3,6 +3,9 @@ import json
 from pathlib import Path
 
 import ollama
+from log import Logger
+
+logger = Logger()
 
 EMBEDDING_MODEL = "hf.co/CompendiumLabs/bge-base-en-v1.5-gguf"
 LANGUAGE_MODEL = "hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF"
@@ -13,7 +16,7 @@ VECTOR_DB_PATH = Path(__file__).resolve().parent.parent / "vector_db.json"
 
 
 def load_dataset():
-    print("Loading the datasets")
+    logger.log("Loading the datasets")
     with open(DATASET_PATH, "r") as file:
         return [line.strip() for line in file.readlines()]
 
@@ -25,17 +28,18 @@ def embed_text(text):
 def build_vector_db(dataset):
     # Each element in the VECTOR_DB will be a tuple (chunk, embedding)
     # The embedding is a list of floats, for example: [0.1, 0.04, -0.34, 0.21, ...]
+    # TODO: Use a more efficient vector database
     vector_db = [(chunk, embed_text(chunk)) for chunk in dataset]
     with open(VECTOR_DB_PATH, "w") as f:
         json.dump(vector_db, f)
-    print(f"Vector database built with {len(vector_db)} entries")
+    logger.log(f"Vector database built with {len(vector_db)} entries")
 
 
 def load_vector_db():
     if VECTOR_DB_PATH.exists():
         with open(VECTOR_DB_PATH, "r") as f:
             vector_db = json.load(f)
-            print(f"Vector database loaded with {len(vector_db)} entries")
+            logger.log(f"Vector database loaded with {len(vector_db)} entries")
             return vector_db
     return []
 
@@ -48,23 +52,21 @@ def cosine_similarity(a, b):
 
 
 def retrieve(query, vector_db, top_n=3):
-    query_embedding = ollama.embed(model=EMBEDDING_MODEL, input=query)["embeddings"][0]
-    # temporary list to store (chunk, similarity) pairs
     similarities = []
     for chunk, embedding in vector_db:
-        similarity = cosine_similarity(query_embedding, embedding)
+        similarity = cosine_similarity(embed_text(query), embedding)
         similarities.append((chunk, similarity))
-    # sort by similarity in descending order, because higher similarity means more relevant chunks
     similarities.sort(key=lambda x: x[1], reverse=True)
-    # finally, return the top N most relevant chunks
     return similarities[:top_n]
 
 
 def chat(query, retrieved_knowledge):
+    # TODO: make a better instruction prompt
     instruction_prompt = (
         """You are a helpful chatbot.\nUse only the following context:\n"""
         + "\n".join([f" - {chunk}" for chunk, _ in retrieved_knowledge])
     )
+    logger.log_task(instruction_prompt, "Instruction Prompt")
     stream = ollama.chat(
         model=LANGUAGE_MODEL,
         messages=[
@@ -73,7 +75,7 @@ def chat(query, retrieved_knowledge):
         ],
         stream=True,
     )
-    print("Chatbot response:")
+    logger.log_rule("Chatbot response:")
     for chunk in stream:
         print(chunk["message"]["content"], end="", flush=True)
     print()
@@ -89,7 +91,7 @@ if __name__ == "__main__":
     input_query = input("Ask me a question: ")
     retrieved_knowledge = retrieve(input_query, vector_db)
 
-    print("Retrieved knowledge:")
+    logger.log_rule("Retrieved knowledge:")
     for chunk, similarity in retrieved_knowledge:
         print(f" - (similarity: {similarity:.2f}) {chunk}")
 
